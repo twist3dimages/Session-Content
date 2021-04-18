b2dad16624c52756658e013deabc6045ce9da90b
Charley <33134502+twist3dimages@users.noreply.github.com>
2021-04-17 21:13:40 -0400

    adding pdf

--- Live Talks/User Group Talks/Making Software Deployments Suck Less/Packages/Adobe_Reader/detect.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module \\MEMBERSRV1\Packages\SoftwareInstallManager
-
-if (Test-InstalledSoftware -Name 'Adobe Reader XI')
-{
-	$true
-}
--- Live Talks/User Group Talks/Making Software Deployments Suck Less/Packages/Company_Webapp/install.ps1
+++
@@ -1,4 +0,0 @@
-Import-Module \\MEMBERSRV1\Packages\SoftwareInstallManager
-Start-Log
-
-New-Shortcut -CommonLocation AllUsersDesktop -Name 'Company Web App' -TargetPath 'http://www.somecompanywebapp.com'
